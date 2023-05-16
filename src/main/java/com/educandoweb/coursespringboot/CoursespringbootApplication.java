15a4379ad1a48fcd99a8fc0b1ee4398dc243156f
Andre Melo <programador.sly@gmail.com>
2023-05-16 10:10:46 -0300

    Project created

---
+++ src/main/java/com/educandoweb/coursespringboot/CoursespringbootApplication.java
@@ -0,0 +1,13 @@
+package com.educandoweb.coursespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursespringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoursespringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/educandoweb/coursespringboot/CoursespringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educandoweb.coursespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursespringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
