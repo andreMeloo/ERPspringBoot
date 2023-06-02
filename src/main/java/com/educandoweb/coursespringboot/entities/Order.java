900ee90b002404d029d7d0700819079daec3cd3d
Andre Melo <programador.sly@gmail.com>
2023-06-02 18:22:12 -0300

    OrderStatus enum

--- src/main/java/com/educandoweb/coursespringboot/entities/Order.java
+++ src/main/java/com/educandoweb/coursespringboot/entities/Order.java
@@ -1,5 +1,6 @@
 package com.educandoweb.coursespringboot.entities;
 
+import com.educandoweb.coursespringboot.entities.entities_enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 import lombok.*;
@@ -10,7 +11,6 @@ import java.time.Instant;
 
 @Getter
 @Setter
-@AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
@@ -19,16 +19,35 @@ public class Order implements Serializable {
 
     @Serial
     private static final long serialVersionUID = 1L;
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @EqualsAndHashCode.Include
     private Long id;
 
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
-    private Instant momento;
+    private Instant moment;
+
+    private Integer orderStatus;
 
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
+
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
+        super();
+        this.id = id;
+        this.moment = moment;
+        setOrderStatus(orderStatus);
+        this.client = client;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        this.orderStatus = orderStatus.getCode();
+    }
 
 }
